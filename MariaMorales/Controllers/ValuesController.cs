53a985725caa246a5142e625e6df85d20e368776
MariaMorales <mariadelcarmenmorales9@gmail.com>
2022-03-12 16:52:44 -0600

    cambios 12/03/22

---
+++ MariaMorales/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using MariaMorales.Data;
+using MariaMorales.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MariaMorales.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly MyDbContext _context;
+
+        public ValuesController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Cliente> Get()
+        {
+            IEnumerable<Cliente> clientes = _context.Cliente.ToList();
+            return clientes;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public Cliente Get(int id)
+        {
+            Cliente clientes = _context.Cliente.Where(a => a.ClienteId == id).FirstOrDefault();
+            if(clientes == null)
+                return new Cliente();
+            return clientes;
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- MariaMorales/Migrations/20220312205608_migracion-cliente9.Designer.cs
+++ MariaMorales/Migrations/20220312205608_migracion-cliente9.Designer.cs
@@ -4,14 +4,16 @@ using MariaMorales.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MariaMorales.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220312205608_migracion-cliente9")]
+    partial class migracioncliente9
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
